fae10f1be991d8cb39e653073fcf274cbb9683c0
inf03 <158447197+inf03egz@users.noreply.github.com>
2025-01-12 14:46:12 +0100

    Update rezerwacja.php
    Signed-off-by: inf03 <158447197+inf03egz@users.noreply.github.com>

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,13 +1,13 @@
 <?php
-	    if(isset($_POST["data"]) && isset($_POST["osoby"]) && isset($_POST["telefon"]) && isset($_POST["stolik"])) {
+	    if(isset($_POST["data"]) && isset($_POST["osoby"]) && isset($_POST["telefon"])) {
 	        $data = $_POST["data"];
 	        $osoby = $_POST["osoby"];
 	        $telefon = $_POST["telefon"];
-			$stolik = $_POST["stolik"]
+		
 
 	        $conn = new mysqli("localhost","root","","baza");
 	
-	        $sql = "INSERT INTO `rezerwacje` (`id`, `nr_stolika`, `data_rez`, `liczba_osob`, `telefon`) VALUES (NULL, '$stolik', '$data', '$osoby', '$telefon');";
+	        $sql = "INSERT INTO `rezerwacje` (`id`, `nr_stolika`, `data_rez`, `liczba_osob`, `telefon`) VALUES (NULL, '1', '$data', '$osoby', '$telefon');";
 	        $result = $conn->query($sql);
 	
 	        $conn -> close();
